d066b7885994469adb8ee7508c01961f18511adb
Sy2n0 <sy2n0@naver.com>
2023-07-08 20:23:17 +0900

    test fix

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'ESCAPE CTF 2023 | HOME' });
 });
 
+router.get('/test', function(req, res, next) {
+  res.render('mobile', { title: 'ESCAPE CTF 2023 | HOME' });
+});
+
 /* Mobile */
 router.get('/mobile', function(req, res, next) {
   res.render('mobile', { title: 'Notice' });
